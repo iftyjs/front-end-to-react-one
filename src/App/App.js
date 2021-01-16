2abf08d2ad0a93bbf50d33c0e6f27b8b2995797d
IFTY <ifty.ahmed0000@gmail.com>
2021-01-16 15:38:04 +0600

    google font added

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,17 @@
 import { Helmet } from "react-helmet";
+import '../assets/css/animate.css';
 import '../assets/css/bootstrap.css';
 import '../assets/css/maicons.css';
 import '../assets/css/mobster.css';
+// import '../assets/css/owl.carousel.min.css';
 import Fav from '../assets/favicon.png';
 import Router from './router';
 function App() {
   return (
     <>
     <Helmet>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Fraunces&family=Source+Sans+Pro&display=swap" rel="stylesheet" />
       <title>Mobster - One page app template</title>
       <link rel="shortcut icon" href={Fav} type="image/x-icon" />
     </Helmet>
